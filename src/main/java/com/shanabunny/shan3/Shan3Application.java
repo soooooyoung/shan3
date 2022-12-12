382e921c0d67106361143dfc3af207a1c3844c81
sooyoungkang <kang.sooyoung@hotmail.com>
2022-12-12 23:15:38 +0900

    intial commit

---
+++ src/main/java/com/shanabunny/shan3/Shan3Application.java
@@ -0,0 +1,13 @@
+package com.shanabunny.shan3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shan3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shan3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanabunny/shan3/Shan3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanabunny.shan3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shan3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
